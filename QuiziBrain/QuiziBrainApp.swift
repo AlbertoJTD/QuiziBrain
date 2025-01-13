e873471bac890d133442f56ac3a3a96e4186af44
AlbertoJTD <albertojtd@outlook.com>
2025-01-12 21:32:24 -0600

    Initial Commit

---
+++ QuiziBrain/QuiziBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuiziBrainApp.swift
+//  QuiziBrain
+//
+//  Created by Alberto Jesus Tepale Diagas on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct QuiziBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuiziBrainTests/QuiziBrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuiziBrainTests.swift
+//  QuiziBrainTests
+//
+//  Created by Alberto Jesus Tepale Diagas on 12/01/25.
+//
+
+import Testing
+@testable import QuiziBrain
+
+struct QuiziBrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
