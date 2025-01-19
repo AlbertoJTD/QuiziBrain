870229a0c03777ba201534410e62903d7ecb8b67
AlbertoJTD <albertojtd@outlook.com>
2025-01-18 23:33:43 -0600

    Update the style of the buttons and add 3 more buttons

--- QuiziBrain/ContentView.swift
+++ QuiziBrain/ContentView.swift
@@ -32,7 +32,45 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped choice 1")
                     }, label: {
-                        Text("Button")
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 1)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 1)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 1)
+                    })
+                    
+                    Button(action: {
+                        print("Tapped choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 1)
                     })
                 }
             }
