9551ed9f9a1d019107187e71840915f3affb1e38
AlbertoJTD <albertojtd@outlook.com>
2025-01-27 07:45:41 -0600

    Update Game model logic

--- QuiziBrain/Models/Game.swift
+++ QuiziBrain/Models/Game.swift
@@ -8,5 +8,42 @@
 import Foundation
 
 struct Game {
+    private(set) var currentQuestionIndex: Int = 0
+    private(set) var guesses = [Question: Int]()
+    private(set) var isOver: Bool = false
+    private let questions: [Question] = Question.allQuestions.shuffled()
     
+    var guessCount: (correct: Int, incorrect: Int) {
+        var count: (correct: Int, incorrect: Int) = (0, 0)
+
+        for (question, guessedIndex) in guesses {
+            if question.correctAnswerIndex == guessedIndex {
+                count.correct += 1
+            } else {
+                count.incorrect += 1
+            }
+        }
+        
+        return count
+    }
+    
+    var numberOfQuestions: Int {
+        questions.count
+    }
+    
+    var currentQuestion: Question {
+        questions[currentQuestionIndex]
+    }
+    
+    mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
+        guesses[currentQuestion] = index
+    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
+    }
 }
